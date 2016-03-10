f9863e3c375dbfe5061873ab2b5987e7bbbdb623
Carlos Avogadro <RedBeard@Carloss-Mac.local>
2016-03-09 21:14:38 -0500

    Added camera view to homeviewController

--- Gaia/HomeViewController.swift
+++ Gaia/HomeViewController.swift
@@ -7,9 +7,17 @@
 //
 
 import UIKit
+import AVFoundation
 
 class HomeViewController: UIViewController {
-
+    
+    //Outlet for camera preview
+    @IBOutlet weak var cameraView: UIView!
+    
+    var session: AVCaptureSession?
+    var stillImageOutput : AVCaptureStillImageOutput?
+    var videoPreviewLayer : AVCaptureVideoPreviewLayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,6 +28,61 @@ class HomeViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+   
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //Resize preview layer
+        videoPreviewLayer?.frame = cameraView.bounds
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        session = AVCaptureSession()
+        session!.sessionPreset = AVCaptureSessionPresetHigh
+        var error: NSError?
+        var input: AVCaptureDeviceInput!
+        //Set capture device to be back camera
+        let backCamera = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        do{
+            //try to access back camera
+            input = try AVCaptureDeviceInput(device: backCamera)
+            
+        }catch let error1 as NSError{
+            error = error1
+            input = nil
+            //print error code
+            print(error?.localizedDescription)
+            
+        }
+        //if no error ocurred and there is an input device accesible add inbput to current session
+        if error == nil && session!.canAddInput(input) {
+            
+            session?.addInput(input)
+            
+            //Set output to be a jpeg
+            stillImageOutput = AVCaptureStillImageOutput()
+            stillImageOutput?.outputSettings = [AVVideoCodecKey: AVVideoCodecJPEG]
+            
+            //If session accepts output
+            if session!.canAddOutput(stillImageOutput){
+                session!.addOutput(stillImageOutput)
+                
+                //Set up video preview from camera into the view
+                videoPreviewLayer = AVCaptureVideoPreviewLayer(session: session)
+                videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspect
+                
+                videoPreviewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.Portrait
+                cameraView.layer.addSublayer(videoPreviewLayer!)
+                
+                //Initiate session
+                session?.startRunning()
+                
+            }
+        }
+      }
     
 
     /*
