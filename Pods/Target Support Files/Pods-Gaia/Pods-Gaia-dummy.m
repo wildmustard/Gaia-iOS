19c4167fd1ac41e1115d27ee87bd9c87479ff252
R. Alex Clark <aclark2089@gmail.com>
2016-03-09 21:45:15 -0500

    Installed podfiles

---
+++ Pods/Target Support Files/Pods-Gaia/Pods-Gaia-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Gaia : NSObject
+@end
+@implementation PodsDummy_Pods_Gaia
+@end
---
+++ Pods/Target Support Files/Pods-Gaia/Pods-Gaia-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_GaiaVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GaiaVersionString[];
+
