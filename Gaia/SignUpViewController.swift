84d3226e6f1015f0a40834117ec889c91a68f647
Carlos Avogadro <RedBeard@carloss-mac.local>
2016-03-31 19:33:27 -0400

    fixed outlet errors

--- Gaia/SignUpViewController.swift
+++ Gaia/SignUpViewController.swift
@@ -16,6 +16,7 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
+    
     @IBOutlet weak var submitButton: UIButton!
     @IBOutlet weak var cancelButton: UIButton!
     
