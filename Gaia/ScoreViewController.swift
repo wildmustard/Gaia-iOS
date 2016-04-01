158935f302b0a657e802efd6e6eac5b63867d89f
Carlos Avogadro <RedBeard@carloss-mac.local>
2016-03-31 23:45:54 -0400

    Added custom nabber and rounded profile picture as POC

--- Gaia/ScoreViewController.swift
+++ Gaia/ScoreViewController.swift
@@ -13,9 +13,20 @@ let userDidLogoutNotification = "User Logged Out\n"
 
 class ScoreViewController: UIViewController {
 
+    @IBOutlet weak var profilePictureImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let profileImage = UIImage(named: "Profile_Picture")
+        
+        var profileImageView = UIImageView(image: profileImage)
+        
+        profilePictureImage.image = roundImage(profileImage!)
 
+        //self.profilePictureImage.layer.cornerRadius = self.profilePictureImage.frame.size.width / 2;
+        self.profilePictureImage.clipsToBounds = true;
+
+        
         // Do any additional setup after loading the view.
     }
 
@@ -28,6 +39,26 @@ class ScoreViewController: UIViewController {
         
         NSNotificationCenter.defaultCenter().postNotificationName(userDidLogoutNotification, object: nil)
 
+    }
+    //Create rounded images
+    func roundImage (image:UIImage) -> UIImage {
+        
+        let size = CGSizeMake(640, 640)
+        UIGraphicsBeginImageContext(size)
+        let ctx = UIGraphicsGetCurrentContext()
+        CGContextAddArc(ctx, 320, 320, 320,0.0, CGFloat(2 * M_PI), 1)
+        CGContextClip(ctx)
+        CGContextSaveGState(ctx)
+        CGContextTranslateCTM(ctx, 0.0, 640)
+        CGContextScaleCTM(ctx, 1.0, -1)
+        CGContextDrawImage(ctx, CGRectMake(0, 0, 640, 640), image.CGImage)
+        CGContextRestoreGState(ctx)
+        let finalImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        
+        UIGraphicsEndImageContext()
+        return finalImage
+        
     }
 
     /*
